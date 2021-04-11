8931bb7419fff19716406fe6f75ad2fa3533c354
Magomed <maga_dahkilgov@outlook.com>
2021-04-12 00:47:51 +0300

    Недоделал Fullpage

--- #src/js/fullpage.js
+++ #src/js/fullpage.js
@@ -4236,17 +4236,3 @@ if(window.jQuery && window.fullpage){
         };
     })(window.jQuery, window.fullpage);
 }
-;
-
-
-$(document).ready(function () {
-    console.log("1")
-  $("#fullpage").fullpage({
-    //options here
-    autoScrolling: true,
-    navigation: true,
-    navigationPosition: "left",
-    scrollBar: false,
-    scrollingSpeed: 500,
-  });
-});
--- #src/js/script.js
+++ #src/js/script.js
@@ -1,1 +1,14 @@
-@@include("alert.js")
+@@include("fullpage.js");
+
+
+$(document).ready(function () {
+    console.log("1")
+  $("#fullpage").fullpage({
+    //options here
+    autoScrolling: true,
+    navigation: true,
+    navigationPosition: "left",
+    scrollBar: false,
+    scrollingSpeed: 500,
+  });
+});
