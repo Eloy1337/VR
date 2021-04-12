cad400d5c6a79af1960335e8e47c1fd582b08bb5
Eloy1337 <malsagov.01@mail.ru>
2021-04-12 17:38:45 +0300

    Сделал попап консультации

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,15 +1,35 @@
 // alert("Hello Gulp1");
 
 $(document).ready(() => {
+
+	// Нажатие на бургер
 	$(".header__burger").click(() => {
 		$(".burgerPopup").animate({
-			top: "0"
-		}, 500)
+			opacity: "1"
+		}, 500).removeClass("hidden");
 	});
 
 	$(".burgerPopup__burger").click(() => {
 		$(".burgerPopup").animate({
-			top: "-100vh"
-		}, 500)
+			opacity: "0"
+		}, 500, () => {
+			$(".burgerPopup").addClass("hidden")
+		});
+	});
+
+	// Получить консультацию
+	$(".header__button").click(() => {
+		$(".formConsultation").removeClass("hidden");
+		$(".formConsultation").animate({
+			opacity: "1"
+		}, 500);
+	})
+
+	$(".formConsultation__left button").click(() => {
+		$(".formConsultation").animate({
+			opacity: "0"
+		}, 500, () => {
+			$(".formConsultation").addClass("hidden")
+		});
 	});
 });
